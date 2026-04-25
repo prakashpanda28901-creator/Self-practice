40fdb3c6e88e3569b7a4b1b1b65b1ba0ff3bb2fb
prakashpanda28901-creator <prakashpanda28901@gmial.com>
2026-04-25 12:04:08 +0530

    add file test case

---
+++ file-writer/file_writer.py
@@ -0,0 +1,6 @@
+def write_to_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+if __name__ == "__main__":
+    write_to_file('output.txt', 'Hello, World!')
+    print('File written successfully.')
