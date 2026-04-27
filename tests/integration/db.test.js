b47784d27dcc24ae9a1ebb065d9e55a8808e7806
prakashpanda28901-creator <prakashpanda28901@gmial.com>
2026-04-27 17:58:57 +0530

    integration test

---
+++ tests/integration/db.test.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { Client } = require('pg');
+describe('Postgrs Integration Test', ()=> {
+    let client;
+    beforeAll(async () => {
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl:false
+        });
+        await client.connect();
+    });
+
+    afterAll(async () => {
+        await client.end();
+    });
+
+    test('should connect and return current timestamp', async()=>{
+        const res = await client.query('SELECT NOW()');
+        expect(res.rowCount).toBe(1);
+        console.log('Current time for DB:', res.row[0].now);
+    });
+
+    test('should create and query a test table', async () =>{
+        await client.query('CREATE TABLE IF NOT EXISTS test_table(id SERIAL PRIMARY KEY, name TEXT)');
+        await client.query('INSERT INTO test_table(name) VALUES ($1)',['hello']);
+        const res = await client.query('SELECT * FROM test_table WHERE name=$1',
+            ['hello']);
+            expect(res.rows.length).toBeGreaterThan(0);
+            expect(res.rows[0].name).toBe('hello');
+    });
+});
